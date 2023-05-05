27a4a5095d23719631c18fc53f29bc818c05aa62
LuckyThief <luckythief13@gmail.com>
2023-05-05 21:24:41 +0400

    Fixed tests

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardDeliveryTest {
@@ -13,36 +16,48 @@ public class AppCardDeliveryTest {
         open("http://localhost:9999");
     }
 
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
+    String planningDate = generateDate(3);
+
     @Test
-    void testFormV1() {
-        $("[data-test-id=city] input").setValue("Самара");
-        $("[data-test-id=date] input").setValue("08 05 2023");
-        $("[data-test-id=name] input").setValue("Гусарский Гусь");
-        $("[data-test-id=phone] input").setValue("+79023004000");
-        $("[data-test-id=agreement]").click();
-        $x("//*[contains(text(), 'Забронировать')]").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(12));
+    void shouldSendForm() {
+        $("[data-test-id='city'] input").setValue("Самара");
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Гусарский Гусь");
+        $("[data-test-id='phone'] input").setValue("+79023004000");
+        $("[data-test-id='agreement']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(12));
+        $("[data-test-id='notification']").shouldBe(visible);
     }
 
     @Test
-    void testFormV2() {
+    void shouldSendFormWithDashInName() {
         $("[data-test-id=city] input").setValue("Казань");
-        $("[data-test-id=date] input").setValue("08 05 2023");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Гусарский-Павловский Гусь Хрустальный");
         $("[data-test-id=phone] input").setValue("+79023004000");
         $("[data-test-id=agreement]").click();
-        $x("//*[contains(text(), 'Забронировать')]").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(12));
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(12));
+        $("[data-test-id='notification']").shouldBe(visible);
     }
 
     @Test
-    void testFormV3() {
+    void shouldSendFormWithLetterYoInName() {
         $("[data-test-id=city] input").setValue("Саратов");
-        $("[data-test-id=date] input").setValue("08 05 2023");
+        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Гусарский Гусёнок");
         $("[data-test-id=phone] input").setValue("+79023004000");
         $("[data-test-id=agreement]").click();
-        $x("//*[contains(text(), 'Забронировать')]").click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(12));
+        $$("button").find(exactText("Забронировать")).click();
+        $("[data-test-id='notification']").shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(12));
+        $("[data-test-id='notification']").shouldBe(visible);
     }
 }
